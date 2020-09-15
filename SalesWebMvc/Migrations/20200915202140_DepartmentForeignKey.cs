89c1e71faaa48e3855a904452540e7bb075ec4d8
Marcelo Hadad <marcelo.hadad@outlook.com>
2020-09-15 18:36:14 -0300

    Foreign key not null (referencial integrity)

---
+++ SalesWebMvc/Migrations/20200915202140_DepartmentForeignKey.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class DepartmentForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Departament_DepartamentId",
+                table: "Seller");
+
+            migrationBuilder.DropTable(
+                name: "Departament");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartamentId",
+                table: "Seller",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartamentId",
+                table: "Seller",
+                column: "DepartamentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartamentId",
+                table: "Seller");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartamentId",
+                table: "Seller",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.CreateTable(
+                name: "Departament",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departament", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Departament_DepartamentId",
+                table: "Seller",
+                column: "DepartamentId",
+                principalTable: "Departament",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
